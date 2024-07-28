1cfe6935aa88586bbd4c080a3715ca7f183f2ad0
neptoo <heywuq@outlook.com>
2024-07-28 20:36:10 +0800

    add 7:slider

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Code 15 React Projects
 
-https://github.com/john-smilga/react-projects
-
-## 组件
+## Component
 
 1.Birthday Reminder 今天有多少人生日
 
-[灵感来源](https://www.uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
+​ [灵感来源](https://www.uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
 
 2.Tours List 旅游新闻列表
 
@@ -14,19 +12,19 @@ https://github.com/john-smilga/react-projects
 
 4.Accordion 折叠手风琴效果
 
-[灵感来源](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
+​ [灵感来源](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
 
 5.Menu 分类列表
 
 6.Side Tabs 侧边栏
 
-bugfix: 修复了api不可用的报错Fix CORS permissions to allow the projects to work from new domain
+bugfix: 修复了 api 不可用的报错
 
-bugfix: index1.module.css 会被 index2.module.css 中的--fix-width 影响，需要将:root 整合到一个文件中
+Fix CORS permissions to allow the projects to work from new domain
 
+> Access to fetch at 'https://course-api.com/react-tours-project' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
 
-
-## 关键文件的注释/notes
+## Project Tree
 
 ```bash
 app.module.css #修改主project的样式
@@ -34,3 +32,8 @@ data.js #修改路由路径、封面图和标题
 index.js #导入组件，配置路由
 ```
 
+## Links
+
+Idea : https://github.com/john-smilga/react-projects
+
+Online Course: Code 15 React Projects - Complete Course
--- src/Page/Slider.js
+++ src/Page/Slider.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./07/data";
 
 import "../index.css";
 import styles from "./07/index7.module.css";
 
 const Slider = () => {
-  return <div className={styles.main}>Slider</div>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  // ?
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+  // autoSlide
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    // ?
+    return () => clearInterval(slider);
+  }, [index]);
+  return (
+    <div className={styles.main}>
+      <section className={styles.section}>
+        <div className={styles.title}>
+          <h2>
+            <span>/</span>reviews
+          </h2>
+        </div>
+        <div className={styles.sectionCenter}>
+          {people.map((person, personIndex) => {
+            const { id, image, name, title, quote } = person;
+            let position = "nextSlide";
+            if (personIndex === index) {
+              position = "activeSlide";
+            }
+            if (
+              personIndex === index - 1 ||
+              (index === 0 && personIndex === people.length - 1)
+            ) {
+              // 条件1：把一个slide挪到左边
+              // 条件2： index为0时 把数组最后一个挪到最左边
+              position = "lastSlide";
+            }
+            return (
+              <article
+                className={`${styles.article} ${styles[position]}`}
+                key={id}
+              >
+                <img src={image} alt={name} className={styles.personImg} />
+                <h4>{name}</h4>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.text}>{quote}</p>
+                <FaQuoteRight className={styles.icon} />
+              </article>
+            );
+          })}
+          <button className={styles.prev} onClick={() => setIndex(index - 1)}>
+            <FiChevronLeft />
+          </button>
+          <button className={styles.next} onClick={() => setIndex(index + 1)}>
+            <FiChevronRight />
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Slider;
